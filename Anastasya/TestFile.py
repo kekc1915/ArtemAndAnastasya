060ae37ee034be26556a7c7e99ddc68f191b68d2
Anastasya <rodichevartem@yandex.ru>
2022-06-14 22:35:53 +0700

    изучение базовых операторов

--- Anastasya/TestFile.py
+++ Anastasya/TestFile.py
@@ -1,1 +1,15 @@
+
+from posixpath import split
+
+
 print('Hello World From Me')
+print('бла бла бла')
+x = 10 #int
+y = 14.2578 #float
+z = 5j #complex
+print (type(x))
+print(x + y + z)
+b = " Артем "
+print(b[2:4])
+a= 'Артем, шоколадка-карамелька'
+print(a.split('-'))
