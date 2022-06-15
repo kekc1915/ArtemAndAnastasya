a54221dfa875ec9bf684e7e6db31d10fce0e4e05
Anastasya <rodichevartem@yandex.ru>
2022-06-15 22:57:13 +0700

    вычисление площади прямоугольника

---
+++ Anastasya/SquareArea.py
@@ -0,0 +1,6 @@
+print ("Введите ширину прямоугольника")
+Width = float(input())
+print("Введите длину прямоугольника")
+Length = float(input())
+print("Площадь прямоугольника равна:", Width*Length)
+
