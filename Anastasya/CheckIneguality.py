e4e54829a5f63dc29866668dfaec52a71cf24409
kekc1915 <43593349+kekc1915@users.noreply.github.com>
2022-06-22 23:19:31 +0700

    Выполнение двух неравеств

--- Anastasya/CheckIneguality.py
+++ Anastasya/CheckIneguality.py
@@ -5,7 +5,13 @@ print ('Введите три вещественных числа')
 CompareOne = float(input('Первое число: ' ))
 CompareTwo = float(input('Второе число: ' ))
 Comparethree = float(input('Третье число: ' ))
+
 if CompareOne < CompareTwo < Comparethree:
-    print ('Неравенство' + CompareOne < CompareTwo < Comparethree + 'выполняется!')
+    print ('Неравенство', CompareOne, '<', CompareTwo, '<', Comparethree, 'выполняется!')
 else:
-    print ('Неравенство не выполняется(')
+    print ('Неравенство' , CompareOne, '<', CompareTwo, '<', Comparethree,'не выполняется!')
+    
+if CompareTwo > CompareOne > Comparethree:
+    print ('Неравенство', CompareTwo, '>', CompareOne, '>', Comparethree, 'выполняется!')
+else:
+    print ('Неравенство', CompareTwo, '>', CompareOne, '>', Comparethree, 'не выполняется!')
